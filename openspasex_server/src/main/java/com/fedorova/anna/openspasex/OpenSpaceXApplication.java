82af3d879cb6a991bc2d763db1c17ff262c6c8d9
Anna Fedorova <anna.fedorova@delightex.com>
2021-07-16 21:34:56 +0200

    fix typo

--- openspasex_server/src/main/java/com/fedorova/anna/openspasex/OpenSpaceXApplication.java
+++ openspasex_server/src/main/java/com/fedorova/anna/openspasex/OpenSpaceXApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenSpaseXApplication {
+public class OpenSpaceXApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(OpenSpaseXApplication.class, args);
+		SpringApplication.run(OpenSpaceXApplication.class, args);
 	}
 }
