ea8b6f41d4f320194eb15996d0e1c9475645f88e
Anna Fedorova <anna.fedorova@delightex.com>
2021-07-16 21:34:14 +0200

    Initial Spring boot web application

---
+++ openspasex_server/src/main/java/com/fedorova/anna/openspasex/OpenSpaseXApplication.java
@@ -0,0 +1,11 @@
+package com.fedorova.anna.openspasex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSpaseXApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSpaseXApplication.class, args);
+	}
+}
